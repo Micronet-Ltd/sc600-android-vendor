43dbdec2a0b8db8102a2e7a9386d2721b1093167
vladimirz <vladimir.zatulovsky@micronet-inc.com>
2022-04-04 09:44:22 +0300

    gc2053 vendor default chromatix

---
+++ qcom/proprietary/mm-camera/mm-camera2/media-controller/modules/sensors/chromatix/0310/chromatix_gc2053/3A/zsl_video/chromatix_gc2053_zsl_video_3a.c
@@ -0,0 +1,27 @@
+/*============================================================================
+
+Copyright (c) 2016 Qualcomm Technologies, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Qualcomm Technologies, Inc.
+
+============================================================================*/
+
+/*============================================================================
+ *                      INCLUDE FILES
+ *===========================================================================*/
+#include "chromatix_common.h"
+#include "chromatix_3a.h"
+
+static chromatix_3a_parms_type chromatix_gc2053_parms = {
+#include "chromatix_gc2053_zsl_video_3a.h"
+};
+
+/*============================================================================
+ * FUNCTION    - load_chromatix -
+ *
+ * DESCRIPTION:
+ *==========================================================================*/
+void *load_chromatix(void)
+{
+  return &chromatix_gc2053_parms;
+}
